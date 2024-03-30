43e966477748371f8547ffa5f55358736a6b888a
ayowaaab <ayoub.dahmen@esprit.tn>
2024-03-30 09:56:15 +0100

    🎨 made it a little bit responsive

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -2,20 +2,33 @@ import "./product.css";
 import { FiShoppingCart } from "react-icons/fi";
 
 const Product = () => {
+  const thumbnail = [
+    "image-product-1-thumbnail.jpg",
+    "image-product-2-thumbnail.jpg",
+    "image-product-3-thumbnail.jpg",
+    "image-product-4-thumbnail.jpg",
+  ];
+  const product = [
+    "image-product-1.jpg",
+    "image-product-2.jpg",
+    "image-product-3.jpg",
+    "image-product-4.jpg",
+  ];
   return (
     <>
       <div className="container">
         <div className="all-images">
+          <img className="arrows" src="icon-next.svg" alt="" />
+          <img className="arrows" src="icon-previous.svg" alt="" />
           <img
             src="image-product-1.jpg"
             alt="mainProduct"
             className="product"
           />
           <div className="thumbnails">
-            <img src="image-product-2-thumbnail.jpg" alt="detail" />
-            <img src="image-product-2-thumbnail.jpg" alt="detail" />
-            <img src="image-product-2-thumbnail.jpg" alt="detail" />
-            <img src="image-product-2-thumbnail.jpg" alt="detail" />
+            {thumbnail.map((el) => (
+              <img src={el} alt="detail" />
+            ))}
           </div>
         </div>
 
@@ -26,18 +39,24 @@ const Product = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
             voluptas dolore, maiores dolorum corrupti voluptatem.
           </p>
-          <h4 className="price">
-            $125.00<mark>50%</mark>
-          </h4>
-          <span className="promo">$250.00</span>
+          <div className="all-price">
+            <h4 className="price">
+              $125.00<mark>50%</mark>
+            </h4>
+            <span className="promo">$250.00</span>
+          </div>
           <div className="shopSection">
             <div className="quantity">
-              <button>-</button>
+              <button>
+                <img src="icon-minus.svg" alt="" />
+              </button>
               <span style={{ padding: ".3rem 1rem" }}>0</span>
-              <button>+</button>
+              <button>
+                <img src="icon-plus.svg" alt="" />
+              </button>
             </div>
             <button className="addToCardBtn">
-              <FiShoppingCart />
+              <img id="shop-card" src="icon-cart.svg" />
               Add to card
             </button>
           </div>
